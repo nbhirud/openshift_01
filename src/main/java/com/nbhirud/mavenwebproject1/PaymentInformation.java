0a42427dace2e335945ccb494e27930ff09b3106
Nikhil Bhirud <nbhirud@gmail.com>
2017-03-20 22:02:10 -0400

    v2

--- src/main/java/com/nbhirud/mavenwebproject1/PaymentInformation.java
+++ src/main/java/com/nbhirud/mavenwebproject1/PaymentInformation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.mavenwebproject1;
+package com.nbhirud.mavenwebproject1;
 
 /**
  *
